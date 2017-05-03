4980979934356dd8e9cfbdd814a2a84e191f5915
t_marianos1 <t_marianos1@verifone.com>
2017-05-03 11:26:12 -0300

    protractor tests

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,14 +1,27 @@
 import { StackbuildersTestPage } from './app.po';
+import { element, by, protractor, browser } from 'protractor';
 
 describe('stackbuilders-test App', () => {
-  let page: StackbuildersTestPage;
+	let page: StackbuildersTestPage;
 
-  beforeEach(() => {
-    page = new StackbuildersTestPage();
-  });
+	beforeEach(() => {
+		page = new StackbuildersTestPage();
+	});
 
-  it('should display message saying app works', () => {
-    page.navigateTo();
-    expect(page.getParagraphText()).toEqual('app works!');
-  });
+	it('Table should have 30 rows as no filter is selected', () => {
+		element.all(by.css('.tbl-content tr')).count().then(function(rowCount) {
+			expect(rowCount).toBe(30);
+		});
+	});
+
+	it('Table should have 30 or less rows as a filter was applied', () => {
+		var selectDropdownElement = element(by.id('mainselection'));
+		selectDropdownElement.all(by.tagName('option')).then(function(options) {
+			options[0].click();
+		});
+
+		element.all(by.css('.tbl-content tr')).count().then(function(rowCount) {
+			expect(rowCount).toBeLessThanOrEqual(30);
+		});
+	});
 });
